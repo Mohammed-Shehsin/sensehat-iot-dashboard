9860b9804aae43ea0b33412f07d8e241608c0a50
Mohammed Shehsin Thamarachalil Abdulresak <115590581+Mohammed-Shehsin@users.noreply.github.com>
2025-07-13 01:19:06 +0200

    Update sensehat_interface.py

--- server/sensehat_interface.py
+++ server/sensehat_interface.py
@@ -1,28 +1,40 @@
 from sense_hat import SenseHat
-import datetime
+from datetime import datetime
 
 sense = SenseHat()
 
 def get_all_sensor_data():
-    t = round(sense.get_temperature(), 2)
-    h = round(sense.get_humidity(), 2)
-    p = round(sense.get_pressure(), 2)
-    o = sense.get_orientation()
-    g = sense.get_gyroscope()
-    a = sense.get_accelerometer()
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return [
-        ("Timestamp", timestamp),
-        ("Temperature (C)", t),
-        ("Humidity (%)", h),
-        ("Pressure (hPa)", p),
-        ("Orientation Pitch (°)", round(o['pitch'], 2)),
-        ("Orientation Roll (°)", round(o['roll'], 2)),
-        ("Orientation Yaw (°)", round(o['yaw'], 2)),
-        ("Gyroscope X (°/s)", round(g['pitch'], 2)),
-        ("Gyroscope Y (°/s)", round(g['roll'], 2)),
-        ("Gyroscope Z (°/s)", round(g['yaw'], 2)),
-        ("Accelerometer X (g)", round(a['pitch'], 2)),
-        ("Accelerometer Y (g)", round(a['roll'], 2)),
-        ("Accelerometer Z (g)", round(a['yaw'], 2)),
-    ]
+    data = []
+
+    try:
+        # Timestamp
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        data.append(("Timestamp", timestamp))
+
+        # Environment
+        data.append(("Temperature (C)", round(sense.get_temperature(), 2)))
+        data.append(("Humidity (%)", round(sense.get_humidity(), 2)))
+        data.append(("Pressure (hPa)", round(sense.get_pressure(), 2)))
+
+        # Orientation (Pitch, Roll, Yaw)
+        orientation = sense.get_orientation()
+        data.append(("Orientation Pitch (°)", round(orientation.get("pitch", 0), 2)))
+        data.append(("Orientation Roll (°)", round(orientation.get("roll", 0), 2)))
+        data.append(("Orientation Yaw (°)", round(orientation.get("yaw", 0), 2)))
+
+        # Gyroscope
+        gyro = sense.get_gyroscope()
+        data.append(("Gyroscope X (°/s)", round(gyro.get("pitch", 0), 2)))
+        data.append(("Gyroscope Y (°/s)", round(gyro.get("roll", 0), 2)))
+        data.append(("Gyroscope Z (°/s)", round(gyro.get("yaw", 0), 2)))
+
+        # Accelerometer
+        accel = sense.get_accelerometer()
+        data.append(("Accelerometer X (g)", round(accel.get("pitch", 0), 2)))
+        data.append(("Accelerometer Y (g)", round(accel.get("roll", 0), 2)))
+        data.append(("Accelerometer Z (g)", round(accel.get("yaw", 0), 2)))
+
+    except Exception as e:
+        data.append(("Error", str(e)))
+
+    return data
